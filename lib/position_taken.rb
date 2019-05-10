17bc8c5e4df17f474dcd2d355b36653ca62ba906
David Cantoran <davidcantoran36@gmail.com>
2019-05-10 00:42:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, idx)
   
-  if board[idx] == " " || board[idx] == "" 
+  if board[idx] == " " || board[idx] == "" || board[idx] == nil
     false 
   elsif board[idx] == "X"
     true 
